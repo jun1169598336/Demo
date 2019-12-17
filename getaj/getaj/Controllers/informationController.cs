5816a727b8b5c9b427c7edda6202e0f25ab0384e
zdh <2289304056@qq.com>
2019-12-17 19:42:47 +0800

    2

---
+++ getaj/getaj/Controllers/informationController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using getajdal;
+using getajdal.model;
+using HPIT.Data.Core;
+using HPIT.Survey.Portal.Filters;
+using Newtonsoft.Json;
+
+namespace getaj.Controllers
+{
+    public class informationController : Controller
+    {
+        // GET: information
+        public ActionResult Index()
+        {
+            List<information> list = Class4.select();
+            return View(list);
+        }
+        public ActionResult insert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult insertmodel(information str)
+        {
+            if (Class4.insert(str) != 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("insert");
+            }
+        }
+
+        public ActionResult update(int id)
+        {
+            return View(Class4.updateli(id));
+        }
+        [HttpPost]
+        public ActionResult updatemodel(information str)
+        {
+            if (Class4.updatemodel(str) != 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("update");
+            }
+        }
+        public ActionResult delete(int id)
+        {
+            Class4.delete(id);
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Action()
+        {
+            return View();
+        }
+    }
+}
---
+++ getaj/getajdal/Class4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HPIT.Data.Core;
+using getajdal.model;
+
+namespace getajdal
+{
+    public class Class4
+    {
+        public static List<information> select()
+        {
+
+            Model1 mo = new Model1();
+            List<information> list = mo.information.ToList();
+            return list;
+
+        }
+        public static int insert(information str)
+        {
+            Model1 mo = new Model1();
+            mo.information.Add(str);
+            return mo.SaveChanges();
+
+        }
+        public static information updateli(int id)
+        {
+
+            Model1 mo = new Model1();
+            return mo.information.FirstOrDefault(p => p.infoId == id);
+        }
+        public static int updatemodel(information str)
+        {
+            Model1 mo = new Model1();
+            mo.Entry(str).State = System.Data.Entity.EntityState.Modified;
+            return mo.SaveChanges();
+        }
+        public static int delete(int id)
+        {
+            Model1 mo = new Model1();
+            information list = mo.information.FirstOrDefault(p => p.infoId == id);
+            mo.Entry(list).State = System.Data.Entity.EntityState.Deleted;
+            return mo.SaveChanges();
+        }
+    }
+}
